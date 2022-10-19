48b8824b8a456fe02d8d5a8c90a1ec45141b8cb5
Tony <tunglin.yang1@gmail.com>
2022-10-19 21:44:34 +0800

    加入專案檔案。

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * int number;
+
+			// 計算數字階乘(Factorial)
+			number = 6;
+			Console.WriteLine($"{number} 的階乘等於 {GetFactorial(number)}");
+
+			number = 20;
+			Console.WriteLine($"{number} 的階乘等於 {GetFactorial(number)}");
+             */
+            Console.WriteLine("請輸入要計算階乘的值");
+            string number = Console.ReadLine();
+            int value = Convert.ToInt32(number);
+            int result = value;
+            
+            for (int i = 1; i < value; i++) 
+            {
+                result *= i;
+                if(result < 0)
+                {
+                    Console.WriteLine("0");
+                    return;
+                }
+            }
+            Console.WriteLine($"{number} 階乘等於{result}");
+
+        }
+    }
+}
